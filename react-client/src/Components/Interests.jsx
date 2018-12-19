9809ad4ee9678ec25989adfeb9035c0ccdeec6bf
James Brady <jlbrady@umich.edu>
2018-12-19 17:51:09 -0500

    Updated after graduation, new resume + fixed typos + added CodeHop

--- react-client/src/Components/Interests.jsx
+++ react-client/src/Components/Interests.jsx
@@ -6,8 +6,8 @@ class Interests extends Component {
         <div className="about">
           <h1 style={{textAlign: 'center'}}>About Me</h1>
           <h3 style={{textAlign: 'center'}}>Hello!</h3>
-          <h3 style={{textAlign: 'center'}}>I am currently studying Computer Science at the University of Michigan and entering my final semester. 
-          I enjoy working with front end systems and learning more about computer networks and cyber security.
+          <h3 style={{textAlign: 'center'}}>I am currently studying Computer Science at the University of Michigan and finishing my final semester. 
+          I enjoy working with both front and back end systems and learning more about computer networks and cyber security.
           While I've been at Michigan for 4 years now, I am originally from Long Island, NY, and I love all things skiing, Univeristy of Michigan sports, and live music.</h3>
         </div>
         <div className="about">
@@ -15,7 +15,7 @@ class Interests extends Component {
             <a href="//www.linkedin.com/in/james-brady-115388115/" target="_blank">LinkedIn Profile</a>
           </h3>
           <h3 style={{textAlign: 'center'}}>Phone: (516)-492-4938</h3>
-          <h3 style={{textAlign: 'center'}}>Email: JLBrady@umich.edu</h3>
+          <h3 style={{textAlign: 'center'}}>Email: jlbrady@umich.edu</h3>
         </div>
       </div>
     )
--- react-client/src/Components/Projects.jsx
+++ react-client/src/Components/Projects.jsx
@@ -11,7 +11,9 @@ class Projects extends Component {
           <h4>Learning Javascript + React</h4>
           <p style={{paddingBottom: '0.5%'}}>In preparation for my previous internship in the summer of 2017 I began to teach myself Javascript and React.  My new position was going to require an understanding of React.js, but I had never used or even learned about Javascript, let alone React.  I began learning some of the syntax/quirks of Javascript and React through the book <u>Learning React</u> by Kirupa Chinnathambi.</p>
           <h4>This Website!</h4>
-          <p>While working in Ann Arbor this past summer I decided to build a website.  My job did not involve as much coding as I would have liked so I decided to take on a side project utilizing some of the skills I had learned that previous semester in my Web Systems course.  The website is built using <em>React, Express,</em> and <em>Webpack</em>, with a bit of <em>Bootstrap CSS</em> for style.  It is hosted via <em>Amazon's S3 storage system</em> and <em>Route 53 cloud DNS service</em>.  Content delivery and is provided by <em>Amazon's CloudFront</em>.  Overall I am happy with the outcome of the project and glad that it has allowed me to touch up on some of my coding skills while learning some of the intracacies of Amazon's cloud computing servives.</p>
+          <p style={{paddingBottom: '0.5%'}}>While working in Ann Arbor this past summer I decided to build a website.  My job did not involve as much coding as I would have liked so I decided to take on a side project utilizing some of the skills I had learned that previous semester in my Web Systems course.  The website is built using <em>React, Express,</em> and <em>Webpack</em>, with a bit of <em>Bootstrap CSS</em> for style.  It is hosted via <em>Amazon's S3 storage system</em> and <em>Route 53 cloud DNS service</em>.  Content delivery is provided by <em>Amazon's CloudFront</em>.  Overall I am happy with the outcome of the project and glad that it has allowed me to touch up on some of my coding skills while learning some of the intracacies of Amazon's cloud computing servives.</p>
+          <h4>CodeHop, an iOS app to prepare for coding interviews on the go!</h4>
+          <p>For our <em>mobile app development for entrepreneurs</em> class our team developed an iOS app to practice studying for coding interviews on the go.  The app is designed to help students quiz themselves on topics commonly covered in technical coding interviews; such as data structures, algorithms, and time complexities.  Main features include instant feedback and answer explanations.  Within one month of uploading to the <em>Apple App Store the app recieved 130+ downloads from 10 different countries</em>.  Throughout the coming semester I plan to continue developing the app by adding new features and content.</p>
           <h2 style={{paddingTop: '1.5%'}}>Internships</h2>
           <h4>Programmatic Mechanics: Software Engineer Intern</h4>
           <p>While interning at Programmatic Mechanics I began development on a web application
@@ -19,11 +21,11 @@ class Projects extends Component {
             Rails application to communicate with the Shopify API.  The Rails application was predominantly used to verify
             account tokens from a SQL database on our backend so that we were able to authorize each Shopify user.</p>
           <h4>Franworth: LAUNCH Team Member</h4>
-          <p style={{paddingBottom: '0.5%'}}>LAUNCH is comprised of a dedicated group of student-leaders who desire the hands on involvement, responsibility,
+          <p style={{paddingBottom: '0.5%'}}>"LAUNCH is comprised of a dedicated group of student-leaders who desire the hands on involvement, responsibility,
             and the opportunity to work alongside some of the brightest minds in the business world, while also acting as helping
             agents to the hundreds of individuals and families that Franworth touches.
             LAUNCH allows you to work alongside high level executives and provides you with INSIGHT into the inner workings of
-            FRANCHISING and the process of building a BRAND.</p>
+            FRANCHISING and the process of building a BRAND."</p>
           <p style={{paddingBottom: '3%'}}>Throughout the internship I led the effort to establish an e-commerce platform from scratch for a new division
              of our company, Liberty Fulfillment.  Our group of 4 people was tasked with planning the project, including both the physical
             and virtual aspects of the creation of the new division.  The physical aspects of the project,
@@ -33,7 +35,7 @@ class Projects extends Component {
             company was expecting rapid growth to 1,500+ franchises within 3 years so automating all systems such as accounting,
             inventory management, and taxation was a priority.  Additional responsibilities included theme selection, integration
             of existing systems, including Quickbooks Online accounting services, and selecting and implementing third party apps necessary to
-            provide a seamless user experience for our clients.</p>
+            provide a seamless user experience for our clients.  Currently the online storefront handles over $1 MM+ in annual sales.</p>
         </div>
       </div>
     )
